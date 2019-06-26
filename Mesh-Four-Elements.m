54ce123c7b881a6eb86fc51d906996b2dfead110
Jiashuo Tong <jtong8@illinois.edu>
2019-06-26 00:50:59 -0500

    20190626-12:50AM

---
+++ Mesh-Four-Elements.m
@@ -0,0 +1,28 @@
+% Node number followed by the x and the y coordinate 
+1 0 0 
+2 0.5 0 
+3 1 0 
+4 0 0.5 
+5 0.5 0.5 
+6 1 0.5 
+7 0 1 
+8 0.5 1 
+9 1 1 
+% Element connectivity information; Element number followed by the first node,
+% second node, third node and fourth node
+1 1 2 5 4 
+2 2 3 6 5 
+3 4 5 8 7 
+4 5 6 9 8 
+% Boundary condition information; Node number followed by "0" or "1" (zero
+% for no boundary condition on that node and "1" for boundary condition on
+% the node), followed by the value of the boundary condition
+1 1 0 
+2 1 0.25 
+3 1 1 
+4 1 0.25 
+5 0 0 
+6 1 1.25 
+7 1 1 
+8 1 1.25 
+9 1 2 
---
+++ Mesh-Sixteen-Elements.m
@@ -0,0 +1,72 @@
+% Node number followed by the x and the y coordinate 
+1 0 0 
+2 0.25 0 
+3 0.5 0 
+4 0.75 0 
+5 1 0 
+6 0 0.25 
+7 0.25 0.25 
+8 0.5 0.25 
+9 0.75 0.25 
+10 1 0.25 
+11 0 0.5 
+12 0.25 0.5 
+13 0.5 0.5 
+14 0.75 0.5 
+15 1 0.5 
+16 0 0.75 
+17 0.25 0.75 
+18 0.5 0.75 
+19 0.75 0.75 
+20 1 0.75 
+21 0 1 
+22 0.25 1 
+23 0.5 1 
+24 0.75 1 
+25 1 1
+% Element connectivity information; Element number followed by the first node,
+% second node, third node and fourth node
+1 1 2 7 6 
+2 2 3 8 7 
+3 3 4 9 8 
+4 4 5 10 9 
+5 6 7 12 11 
+6 7 8 13 12 
+7 8 9 14 13 
+8 9 10 15 14 
+9 11 12 17 16 
+10 12 13 18 17 
+11 13 14 19 18 
+12 14 15 20 19 
+13 16 17 22 21 
+14 17 18 23 22 
+15 18 19 24 23 
+16 19 20 25 24 
+% Boundary condition information; Node number followed by "0" or "1" (zero
+% for no boundary condition on that node and "1" for boundary condition on
+% the node), followed by the value of the boundary condition
+1 1 0 
+2 1 0.0625 
+3 1 0.25 
+4 1 0.5625 
+5 1 1 
+6 1 0.0625 
+7 0 0 
+8 0 0 
+9 0 0 
+10 1 1.0625 
+11 1 0.25 
+12 0 0 
+13 0 0 
+14 0 0 
+15 1 1.25 
+16 1 0.5625 
+17 0 0 
+18 0 0 
+19 0 0 
+20 1 1.5625 
+21 1 1 
+22 1 1.0625 
+23 1 1.25 
+24 1 1.5625 
+25 1 2 
